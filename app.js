62e831024db87294489543729bcfab81b686a188
Regina Putri <reginaputria2003@gmail.com>
2021-01-19 06:47:24 +0700

    add response

--- app.js
+++ app.js
@@ -2,37 +2,40 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const cors = require('cors')
 const nodemailer = require('nodemailer')
-const PORT = 4000
+const PORT = process.env.PORT
 
-let transporter = nodemailer.createTransport({
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.listen(PORT, () => { 
+    console.log(`Server started on port ${PORT}`) 
+})
+
+const transporter = nodemailer.createTransport({
     service: process.env.MAILER_SERVICE_PROVIDER,
     auth: {
-        user: process.env.MAILER_EMAIL_ID,
-        pass: process.env.MAILER_PASSWORD
+        user: process.env.YOUR_EMAIL,
+        pass: process.env.YOUR_PASSWORD
     }
 });
 
 app.post('/send-email', (req, res) => {
-    // const email = req.body.email
-
+    const email = req.body.email
     const mailOptions = {
-        from: process.env.MAILER_EMAIL_ID,
-        to: 'reginaputria2003@gmail.com',
+        from: process.env.YOUR_EMAIL,
+        to: email,
         subject: 'Notification',
         html: '<h1>Hai, Selamat Datang</h1> <p>Selamat Bersenang-senang!</p>'
     }
 
     transporter.sendMail(mailOptions, (err, info) => {
+        const data = {
+            'status': 200,
+            'values': 'Check Your Email!'
+        };
+        
         if (err) throw err
         console.log('Email sent: ' + mailOptions.to + ' ' + info.response)
+        res.json(data)
       })
 });
-
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(cors())
-app.listen(PORT, () => { 
-    console.log(`Server started on port ${PORT}`) 
-})
